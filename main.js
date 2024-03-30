fbd0dbd4d41679984c2181d4efbf7c558b82e411
Bapt5 <63651713+Bapt5@users.noreply.github.com>
2024-03-30 16:35:15 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// var p = 0;
+// var offset = 0;
+//
+//
+// window.addEventListener('scroll', () => {
+//   p = $( ".contact" ).last();
+//   offset = p.offset();
+//   p1 = $( ".contact" ).first();
+//   position = p.position();
+//   $('.contact').css('bottom', "50%");
+//   $('.contact').css('left', offset.left);
+//   console.log("left :" + offset.left + "top :" + offset.top);
+//   console.log(offset);
+//   console.log(position);
+//
+//   if (window.scrollY > 300) {
+//     $('.contact').addClass('scroll');
+//     $('.contact').css('bottom', '2%');
+//     $('.contact').css('right', '2%');
+//     $('.contact').css('top', '');
+//     $('.contact').css('left', '');
+//
+//     $('.txt_reseau').addClass('scroll');
+//   } else {
+//     $('.contact').removeClass('scroll');
+//     $('.txt_reseau').removeClass('scroll');
+//     $('.contact').css('bottom', '');
+//     $('.contact').css('right', '');
+//     $('.contact').css('top', '');
+//     $('.contact').css('left', '');
+//   }
+// });
+//
+//
+//
+//
+// $('#subscribe').submit(function() {
+//   console.log('sub');
+// });
